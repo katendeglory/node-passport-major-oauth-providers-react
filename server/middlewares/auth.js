45dc5290cce857c8162cf993abcdaf4425b4e6e1
Glory Katende <katendeglory@gmail.com>
2020-07-20 22:11:33 +0200

    Yet another commit

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,11 +1,11 @@
 const auth = {
   isAuth: (req, res, next) => {
     if (req.isAuthenticated()) next();
-    else res.redirect("/");
+    else res.status(401).json({ message: "You are not logged in" });
   },
   isAdmin: (req, res, next) => {
-    if (req.user.role === 'Admin') next();
-    else res.redirect("/");
+    if (req.isAuthenticated() && req.user.role === 'Admin') next();
+    else res.status(401).json({ message: "You are not authorized to view this ressource, Admin privileges required" });
   },
 }
 
